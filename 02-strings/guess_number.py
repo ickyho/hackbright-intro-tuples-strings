f87eafdb7b904b0bb03646b460907ce98bf56eb9
test111 <test@gmail.com>
2015-11-30 21:19:49 -0800

    you win!

--- 02-strings/guess_number.py
+++ 02-strings/guess_number.py
@@ -1,12 +1,12 @@
 import random
-randnum = random.randint(0,10)
-
-userguess = raw_input("Guess a number between 1 and 10 ")
+randnum = random.randint(1,10)
+print randnum
+userguess = int(raw_input("Guess a number between 1 and 10 "))
 while randnum != userguess:
 	if randnum > userguess:
 		print "too low!"
-		userguess = raw_input("Guess a number between 1 and 10 ")
+		userguess = int(raw_input("Guess a number between 1 and 10 "))
 	if randnum < userguess:
 		print "too high!"
-		userguess = raw_input("Guess a number between 1 and 10 ")
+		userguess = int(raw_input("Guess a number between 1 and 10 "))
 print "you win!"
