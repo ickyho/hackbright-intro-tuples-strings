e6bc50b3cf7ead8852b94ca63b74ef7aca9e3320
test111 <test@gmail.com>
2015-11-30 20:09:12 -0800

    one fish list

--- 02-strings/parse_strings.py
+++ 02-strings/parse_strings.py
@@ -1,8 +1,11 @@
 my_name = "vicky"
 name_list = []
-
 for letter in my_name:
 	name_list.append(letter)
-
 print name_list
 
+str = "1,2,3,4,5"
+print str.split(",")
+
+string = "One fish two fish red fish blue fish"
+print string.split(" ")
