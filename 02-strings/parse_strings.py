c9222bc2a82931ff10edba8fe8dd8f3111ccc0cc
test111 <test@gmail.com>
2015-11-30 20:57:52 -0800

    total is 20.49

--- 02-strings/parse_strings.py
+++ 02-strings/parse_strings.py
@@ -9,3 +9,12 @@ print str.split(",")
 
 string = "One fish two fish red fish blue fish"
 print string.split(" ")
+
+items = ["item:apples,quantity:4,price:1.50\n", "item:pears,quantity:5,price:2.00\n", "item:cereal,quantity:1,price:4.49\n"]
+sum = 0
+for thing in items:
+	split_str = thing.split(",")
+	quantity = float(split_str[1].split(":")[1])
+	price = float(split_str[2].split(":")[1])
+	sum += price * quantity
+print sum
